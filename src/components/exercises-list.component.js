1d01edd5293eb55c3e44072c37ddd15b9a0916d7
p5f8 <p5f8@hotmail.com>
2019-07-24 17:55:54 -0300

    create view list exercises

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit/"+props.exercise._id}>Edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+        </td>
+    </tr>
+);
 
 export default class ExercisesList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = {
+            exercises: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises')
+             .then(response => {
+                 this.setState({
+                     exercises : response.data
+                 });
+             })
+             .catch(error => {
+                 console.log('Error fetch all exercises: ' + error);
+             });
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/' + id)
+             .then( res => console.log(res.data));
+        
+        this.setState({
+            exercises : this.state.exercises.filter(el => el._id !== id)
+        });
+    }
+
+    exercisesList() {
+        return this.state.exercises.map(currentExercise => {
+            return <Exercise exercise = {currentExercise} deleteExercise={this.deleteExercise}
+                        key={currentExercise._id} />
+        })
+
+
+    }
 
     render() {
         return (
             <div>
-                <b>You are on the Exercises List Component</b>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thread className="thread-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        { this.exercisesList() }
+                    </tbody>
+                </table>
             </div>
         );
     }
